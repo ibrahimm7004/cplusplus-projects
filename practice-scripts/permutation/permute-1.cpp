3be45f6d503c145813c1f32c8a7127d756d5a7aa
ibrahimm7004 <ibrahim.mahmood57@gmail.com>
2025-03-20 05:02:12 +0500

    adding files

---
+++ practice-scripts/permutation/permute-1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int factorial(int n) {
+    if (n > 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
+}
+
+int main()
+{
+	int n, noOfPerm;
+	vector <int> vec;
+	
+	cout<<"Enter size N of vector: ";
+	cin>>n;
+	
+	noOfPerm = factorial(n);
+	
+	cout << "\n" << noOfPerm << "\n";
+	
+	return 0;
+ 	
+}
